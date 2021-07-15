0502c41073b8b3d10a09bbd086cc3ec4b797aeb3
kate-LAB-HP <18810809501@163.com>
2021-07-15 20:16:38 +0800

    修复hello.py

--- test/hello.py
+++ test/hello.py
@@ -1,26 +1,5 @@
 from flask import Flask,render_template
-from flask_mysqldb import MySQL
-
 app = Flask(__name__)
-app.config['MYSQL_USER'] = 'socialcontact'
-app.config['MYSQL_PASSWORD'] = 'socialcontact123'
-app.config['MYSQL_DB'] = 'socialcontact'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-mysql = MySQL(app)
-@app.route('/mysql')
-def users():
-    cur = mysql.connection.cursor()
-    cur.execute('''SELECT user, host FROM mysql.user''')
-    rv = cur.fetchall()
-    return str(rv)
-
 @app.route('/')
 def hello():
-<<<<<<< HEAD
     return render_template('admin/index.html')
-
-if __name__ == '__main__':
-    app.run()
-=======
-    return render_template('login.html')
->>>>>>> b7295ec5def8f34901818a5558380049e2a143a7
