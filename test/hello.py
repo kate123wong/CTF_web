186b1f97d475ebd08b77185740e56b187f1b65dd
kate-LAB-HP <18810809501@163.com>
2021-07-16 23:02:37 +0800

    前后端数据库端联通版本

--- test/hello.py
+++ test/hello.py
@@ -1,7 +1,9 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template, request
+import requests
 import pymysql
 import config
 import base64
+import json
 
 conn=pymysql.connect(host = config.host # 连接名称，默认127.0.0.1
 ,user = 'root' # 用户名
@@ -17,18 +19,21 @@ app = Flask(__name__)
 def hello():
     return render_template('login.html')
 
+@app.route('/login',methods=['GET'])
+def tologin():
+    return render_template('login.html')
 @app.route('/login',methods=['POST'])
 def login():
 
+    status = 210
     #获取登陆的用户名和密码
     username = request.form.get('username')
     passwd = request.form.get('passwd')
-
+    
     #在数据库中查询该用户名是否注册已经密码是否正确
     cur = conn.cursor()
     sql="select * from Users where username = %s"
     params = (username)
-    print(sql,params)
     try:
         cur.execute(sql,params) # 执行查询的sql语句
         data = cur.fetchall()
@@ -39,19 +44,24 @@ def login():
     #如果没有注册，进入注册页面
     if len(data) == 1:
         if data[0][2] == passwd:
-            print("login in ok.")
-            return render_template('index.html')
+            status = 206
         else:
-            return "passwd is not right, please try again!"
-
+            status = 205
     else:
-        print("username",username,"has not  registered!")
-        return render_template('register.html')
+        # print("username",username,"has not  registered!")
+        # return render_template('register.html')
+        #html = render_template('register.html')
+        status = 201
+    return json.dumps({ 'status': status})
 
+@app.route('/register',methods=['GET'])
+def toregister():
+    return render_template('register.html')
 
 @app.route('/register',methods=['POST'])
 def register():
 
+    status = 210 #其他状态
     #获取注册的用户名和密码
     username = request.form.get('username')
     passwd = request.form.get('passwd')
@@ -59,35 +69,40 @@ def register():
     #在数据库中查询该用户名是否已经被注册
     cur = conn.cursor()
     sql="select * from Users where username = %s"
+    #sql="select * from Users where username = '" + username + "';"
     params = (username)
-    #print(sql,params)
+    #print(sql)
+    data = tuple()
     try:
         cur.execute(sql,params) # 执行查询的sql语句
+        #cur.execute(sql) # 执行查询的sql语句
         data = cur.fetchall()
         conn.commit() # 提交到数据库执行
         cursor.close()
     except:
         conn.rollback()# 如果发生错误则回滚
     #the username haven't been registered
-    #print(len(data))
+    print(data)
     if len(data) == 0:
         regist = 'insert into Users (username,3passwd2) values (%s, %s)'
+        print(regist)
         params_regist = (username,passwd)
         try:
             cur.execute(regist,params_regist)
             conn.commit()
-            print(username ," has benn registered.")
-            return render_template('login.html')
+            status = 202 #注册成功
+            print("success!")
         except:
-            print("something is error.")
             conn.rollback()
+            status = 203 #注册失败
     else:
-        print("username",username,"has been registered!")
-        return "username %s has been registered! please try again" %username
-
+        status = 204 #用户已被注册
+    return json.dumps({'status': status})
 
 @app.route('/index',methods=['POST'])
 def index():
+
+    status = 210
     username = request.form.get('username')
     passwd = request.form.get('passwd')
     print(username, passwd)
@@ -111,4 +126,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+   app.run()
