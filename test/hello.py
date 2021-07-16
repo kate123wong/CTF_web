c70adb519c0fa03d100bb3bb7a728ed49e52eedf
kate-LAB-HP <18810809501@163.com>
2021-07-16 11:00:53 +0800

    1.3

--- test/hello.py
+++ test/hello.py
@@ -1,11 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('login.html')
 @app.route('/register')
+@app.route('/register',methods=['POST'])
 def register():
-    username = request.args.get('username')
-    passwd = request.args.get('passwd')
-    return render_template('register./html')
-
+    #username = request.form.get('username')
+    #passwd = request.form.get('passwd')
+    return render_template('register.html')
+#if  __main__ == '__main__':
+#    app.run()
+if __name__=="__main__":
+    app.run(host = "0.0.0.0",port = 5005, debug = True)
