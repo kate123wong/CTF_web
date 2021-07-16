b5de6c801e568477b0f936a3e7e512da798dc00d
jerrymajerry <1339442006@qq.com>
2021-07-16 14:35:30 +0800

    hello.py and login.html disconnect

--- test/hello.py
+++ test/hello.py
@@ -1,7 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__)
 @app.route('/')
 def hello():
+    return render_template('login.html')
+#@app.route('/register')
+@app.route('/login',methods=['POST'])
+def register():
+    username = request.form.get('username')
+    passwd = request.form.get('password')
+    
+    print(username, passwd)
+
     return render_template('index.html')
     
 if __name__ == "__main__":
